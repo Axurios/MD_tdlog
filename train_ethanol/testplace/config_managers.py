8cc98352d1bed5a82e9d40dd9246e9216f261fff
Clovis Lapointe <lapointe@localhost.localdomain>
2025-05-23 15:55:15 +0200

    xml manager for multi calculations

--- train_ethanol/testplace/config_managers.py
+++ train_ethanol/testplace/config_managers.py
@@ -3,8 +3,9 @@ import h5py
 import xml.etree.ElementTree as ET
 
 from h5py import File 
-from typing import Dict, Any
+from typing import Dict, Any, List, Tuple
 import numpy as np
+import distutils
 
 class H5Manager : 
     def __init__(self, path_h5 : os.PathLike[str], mode : str = "writing") :
@@ -242,31 +243,123 @@ class XMLManager :
 
         self.tree = ET.ElementTree(self.root)
         self.tree.write(self.path_xml)
+        return
+
+def _pretty_print(current, parent=None, index=-1, depth=0):
+    for i, node in enumerate(current):
+        _pretty_print(node, current, i, depth + 1)
+    if parent is not None:
+        if index == 0:
+            parent.text = '\n' + ('\t' * depth)
+        else:
+            parent[index - 1].tail = '\n' + ('\t' * depth)
+        if index == len(parent) - 1:
+            current.tail = '\n' + ('\t' * (depth - 1))
+
+class MetaXML : 
+    def __init__(self, path_xml : os.PathLike[str], 
+                 list_folder : List[os.PathLike[str]] = [],
+                 mode : str = "writing") :
+        """H5Manager object for: writing and reading
+        
+        Parameters
+        ----------
+
+        path_h5 : os.PathLike[str]
+            Path to .h5 file to write / read
+
+        mode : str 
+            Writing or reading mode
+        """
+        self.path_xml = path_xml
+        self.list_folder = list_folder
+        self.mode = mode 
+        self.equiv_dic = {}
+
+        if mode == "writing" :
+            self.init_xml()
+        elif mode == "reading" : 
+            self.read_xml()
+
+    def init_xml(self) -> None : 
+        """Init .xml file for writing"""
+        self.tree = None 
+        self.root = ET.Element('MetaFolder')
+        for i, path in enumerate(self.list_folder) : 
+            path_ET = ET.SubElement(self.root, f'id{str(i)}', name=path)
+            ET.SubElement(path_ET, 'launch').text = 'False'
+            ET.SubElement(path_ET, 'finish').text = 'False'
+            ET.SubElement(path_ET, 'analyse').text = 'False'
         return 
     
-# ###############################
-# # INPUTS 
-# ###############################
-# data = {"Into":1.0,
-#         "The":2.0,
-#         "Storm":'a'}
-# path_xml = './test.xml'
+    def write_xml(self) -> None :
+        _pretty_print(self.root)
+        self.tree = ET.ElementTree(self.root)
+        self.tree.write(self.path_xml)
+        return 
 
-# data_h5 = {"Into":1.0,
-#            "The":np.array([2.0]),
-#            "Storm":'a'}
-# path_h5 = './test.h5'
-# ################################
+    def read_xml(self) -> None : 
+        """Init .xml file reading"""
+        self.tree = ET.parse(self.path_xml)
+        self.root = self.tree.getroot()
+        return
+    
+    def parse_xml(self) -> Dict[str, Dict[str, Any]] : 
+        """Parse .xml file 
+        
+        Returns 
+        -------
 
-# xml_obj = XMLManager(path_xml, mode='writing')
-# xml_obj.generate_xml(data)
+        Dict[str, Any]
+            Data contain in .xml file
+        """
+        def str2bool(v):
+            if v in ("true", "True", ".True.") : 
+                return True
+            elif v in ('false', 'False', '.False.') :
+                return False
 
-# xml_obj2 = XMLManager(path_xml, mode='reading')
-# print(xml_obj2.parse_xml()) 
+        path_dic = {}
+        for child in self.root :
+            sub_dic = {}
+            for c in child :  
+                sub_dic[c.tag] = str2bool(c.text.strip())
+            
+            path_dic[child.attrib['name']] = sub_dic
 
-# h5_obj = H5Manager(path_h5, mode='writing')
-# h5_obj.add_or_update_data('TimeOfFailure', data_h5)
-# h5_obj.close()
+            self.equiv_dic[child.attrib['name']] = child.tag
 
-# h5_obj2 = H5Manager(path_h5, mode='reading')
-# print(h5_obj2.extract_data('TimeOfFailure'))
+        return path_dic
+
+    def update_xml(self, data : Dict[str, Dict[str, Any]]) -> None : 
+        """Generate .xml file from data 
+        
+        Parameters
+        ----------
+
+        data : Dict[str, Any]
+            Data to store in .xml file
+        """
+        self.root = ET.Element('MetaFolder')
+        for key, val in data.items() :
+            path_ET =  ET.SubElement(self.root, self.equiv_dic[key], name=key)
+            for sub_key, sub_val in val.items() : 
+                ET.SubElement(path_ET, sub_key).text = str(sub_val)
+
+        return
+    
+    def select_calculation_to_run(self, dic_path : Dict[str, Dict[str,Any]], 
+                                  nb_calc : int) -> Tuple[ List[os.PathLike[str]], Dict[str, Dict[str,Any]] ] :
+        dic_path_update = dic_path.copy()
+        possible_path2launch = [ key for key, val in dic_path.items() if val['launch'] == False ]
+
+        if len(possible_path2launch) <= nb_calc : 
+            path2launch = possible_path2launch
+        else : 
+            array_index = np.arange(len(possible_path2launch))
+            id2launch = np.random.choice(array_index, nb_calc)
+            path2launch = [possible_path2launch[id] for id in id2launch]
+
+        for path in path2launch : 
+            dic_path_update[path]['launch'] = True
+        return path2launch, dic_path_update
